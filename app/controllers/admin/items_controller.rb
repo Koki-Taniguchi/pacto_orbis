7796f274e0245f3a055a3693db5d344f508afa02
kouki.taniguchi <kouki.taniguchi>
2019-07-28 21:36:17 +0900

    [add]action

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,32 @@
 class Admin::ItemsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+    @item = Item.new
+    @artists = Artist.all
+    @labels = Label.all
+    @genles = Genle.all
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :price, :status, :stock, :jacket_image_id)
+  end
 end
