defa989922225e8e7c149d0aa07b2d2b314d3b07
kouki.taniguchi <kouki.taniguchi>
2019-08-21 01:44:15 +0900

    [add]enduser/paginate

--- app/controllers/users/items_controller.rb
+++ app/controllers/users/items_controller.rb
@@ -1,6 +1,6 @@
 class Users::ItemsController < Users::Base
   def index
-    @items = Item.where(status: true)
+    @items = Item.where(status: true).page(params[:page]).per(10)
   end
 
   def show
--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -1,7 +1,7 @@
 class Users::OrdersController < Users::Base
   before_action :authenticate_user!
   def index
-    @orders = current_user.orders
+    @orders = current_user.orders.page(params[:page]).per(10)
   end
 
   def new
